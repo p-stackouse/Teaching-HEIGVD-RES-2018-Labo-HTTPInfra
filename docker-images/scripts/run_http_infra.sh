2e3e2195022f21641dfc7b79ea4eaf986db642e4
Patrick Netols <patrick.neto@protonmail.com>
2018-06-11 18:53:13 +0200

    New index bootstrap index page for static apache server

--- docker-images/scripts/run_http_infra.sh
+++ docker-images/scripts/run_http_infra.sh
@@ -6,6 +6,11 @@ docker kill apache_static apache_reverse express_dynamic
 #Remove containers created before with the same name
 docker rm apache_static apache_reverse express_dynamic
 
+#Re building containers
+docker build -t res/apache_php /Users/patrickneto/CloudStation/Heig-VD/RES/Labos/Teaching-HEIGVD-RES-2018-Labo-HTTPInfra/docker-images/apache-php-image
+docker build -t res/express_students /Users/patrickneto/CloudStation/Heig-VD/RES/Labos/Teaching-HEIGVD-RES-2018-Labo-HTTPInfra/docker-images/express-image
+docker build -t res/apache_rp /Users/patrickneto/CloudStation/Heig-VD/RES/Labos/Teaching-HEIGVD-RES-2018-Labo-HTTPInfra/docker-images/apache-reverse-proxy
+
 #Run containers
 docker run -d --name apache_static res/apache_php
 docker run -d --name express_dynamic res/express_students
